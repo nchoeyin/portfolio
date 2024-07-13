3f55fb664f2897d601da16889fe9c86b10b21e8c
Ngawang Choeying <ngawangchoeying303@gmail.com>
2024-07-13 18:03:25 +0530

    Responsive header improvisation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,20 +6,20 @@ import { CommonModule, NgIf } from '@angular/common';
   imports: [CommonModule, NgIf],
   template: `
   <section id="navbar" class="flex justify-between h-40 items-center bg-white">
-    <div class="text-md sm:text-3xl md:text-4xl text-black text-center font-sm">Ngawang Choeying</div>
+    <div class="text-md sm:text-2xl md:text-4xl text-black text-center font-sm">Ngawang Choeying</div>
      <section class="flex gap-4 items-center">
       <span *ngIf="!showMenu"(click)="showMenu =! showMenu" class="material-symbols-outlined text-md sm:text-3xl md:hidden lg:hidden text-black cursor-pointer">menu</span>
       <section class="md:flex gap-6 {{showMenu ? 'block' : 'hidden'}}">
-        <div class="text-xs sm:text-lg md:text-2xl lg:text-3xl text-black hover:text-gray-200">
+        <div class="text-xs sm:text-lg lg:text-2xl text-black hover:text-gray-200">
           <a href="#experience">Experience</a>
         </div>
-        <div class="text-xs sm:text-xl md:text-lg lg:text-3xl text-black hover:text-gray-200">
+        <div class="text-xs sm:text-xl lg:text-2xl text-black hover:text-gray-200">
           <a href="#skills">Skills</a>
         </div>
-        <div class="text-xs sm:text-xl md:text-lg lg:text-3xl text-black hover:text-gray-200">
+        <div class="text-xs sm:text-xl lg:text-2xl text-black hover:text-gray-200">
           <a href="#projects">Projects</a>
         </div>
-        <div class="text-xs sm:text-xl md:text-lg lg:text-3xl text-black hover:text-gray-200">
+        <div class="text-xs sm:text-xl lg:text-2xl text-black hover:text-gray-200">
           <a href="#contact">Contact</a>
         </div>
       </section>
