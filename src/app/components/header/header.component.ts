afd509e9f35ca7e7a8425164bcc78b502bd07201
Ngawang Choeying <ngawangchoeying303@gmail.com>
2024-07-13 13:49:57 +0530

    Hamburger and clear icon implemented for responsive header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,32 +1,33 @@
 import { Component } from '@angular/core';
-
+import { CommonModule, NgIf } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgIf],
   template: `
   <section id="navbar" class="flex justify-between h-40 items-center bg-white">
-    <div class="hidden text-3xl md:text-4xl lg:text-5xl text-black text-center font-sm">Ngawang Choeying</div>
-    <section class="hidden md:flex gap-6">
-      <div class="sm:text-xl md:text-2xl lg:text-3xl text-black hover:text-gray-200">
+    <div class="text-md sm:text-3xl md:text-4xl text-black text-center font-sm">Ngawang Choeying</div>
+    <span *ngIf="!showMenu"(click)="showMenu =! showMenu" class="material-symbols-outlined text-md sm:text-3xl md:hidden lg:hidden text-black cursor-pointer">menu</span>
+    <section class="md:flex gap-6 {{showMenu ? 'block' : 'hidden'}}">
+      <div class="text-sm sm:text-lg md:text-2xl lg:text-3xl text-black hover:text-gray-200">
         <a href="#experience">Experience</a>
       </div>
-      <div class="sm:text-xl md:text-2xl lg:text-3xl text-black hover:text-gray-200">
+      <div class="text-md sm:text-xl md:text-lg lg:text-3xl text-black hover:text-gray-200">
         <a href="#skills">Skills</a>
       </div>
-      <div class="sm:text-xl md:text-2xl lg:text-3xl text-black hover:text-gray-200">
+      <div class="text-md sm:text-xl md:text-lg lg:text-3xl text-black hover:text-gray-200">
         <a href="#projects">Projects</a>
       </div>
-      <div class="sm:text-xl md:text-2xl lg:text-3xl text-black hover:text-gray-200">
+      <div class="text-md sm:text-xl md:text-lg lg:text-3xl text-black hover:text-gray-200">
         <a href="#contact">Contact</a>
       </div>
     </section>
+    <span *ngIf="showMenu"(click)="showMenu =! showMenu" class="material-symbols-outlined text-md sm:text-3xl md:hidden lg:hidden text-black cursor-pointer">clear</span>
 </section>
-
 
   `,
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+showMenu = false;
 }
