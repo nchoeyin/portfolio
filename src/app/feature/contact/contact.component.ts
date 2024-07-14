ad140273b24b72d61d1a001dce5bc1c84456ff76
Ngawang Choeying <ngawangchoeying303@gmail.com>
2024-07-14 11:21:23 +0530

    Responsive contact page implemented

--- src/app/feature/contact/contact.component.ts
+++ src/app/feature/contact/contact.component.ts
@@ -5,13 +5,13 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <section class="flex flex-col gap-5 my-10">
-        <h1 class="text-bold text-5xl text-black text-center">Get in Touch</h1>
-        <p class="text-xl text-center text-black">Contact Me</p>
+    <section class="flex flex-col gap-2 lg:gap-5 my-5 lg:my-10">
+        <h1 class="text-bold text-lg lg:text-5xl text-black text-center">Get in Touch</h1>
+        <p class="text-md lg:text-xl text-center text-black">Contact Me</p>
     </section>
-    <section class="flex justify-center mb-10">
-      <section class="border border-gray-400 rounded-xl w-2/5 p-5">
-        <p class="text-xl text-center text-black">ngawangchoeying303<span class="text-xl">&#64;</span>gmail.com</p>
+    <section class="flex justify-center mb:4 lg:mb-10">
+      <section class="border border-gray-400 rounded-xl lg:w-2/5 p-2 lg:p-5">
+        <p class="text-md lg:text-xl text-center text-black">ngawangchoeying303<span class="text-xl">&#64;</span>gmail.com</p>
       </section>
     </section>
   `,
