a88f90af25f622a336b68f1ed6505626cee7d830
Ngawang Choeying <ngawangchoeying303@gmail.com>
2024-07-14 11:33:34 +0530

    Responsive contact page implemented

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,18 +5,18 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-  <footer class="flex flex-col gap-10 my-10">
+  <footer class="flex flex-col gap-2 my-4 lg:gap-10 lg:my-10">
   <nav>
-    <div class="">
+    <div class="hidden lg:block">
       <ul class="flex gap-5 justify-center">
         <li><a class="text-3xl text-black hover:text-gray-200" href="#experience">Experience</a></li>
         <li><a class="text-3xl text-black hover:text-gray-200" href="#experience">Skills</a></li>
         <li><a class="text-3xl text-black hover:text-gray-200" href="#projects">Projects</a></li>
         <li><a class="text-3xl text-black hover:text-gray-200" href="#contact">Contact</a></li>
-      </ul>
+      </ul> 
     </div>
   </nav>
-  <p class="text-center text-xl text-black mb-10">Copyright &#169; 2024 Ngawang Choeying. All Rights Reserved.</p>
+  <p class="text-center text-sm lg:text-xl text-black mb-10">Copyright &#169; 2024 Ngawang Choeying. All Rights Reserved.</p>
 </footer>
   `,
   styleUrl: './footer.component.scss'
