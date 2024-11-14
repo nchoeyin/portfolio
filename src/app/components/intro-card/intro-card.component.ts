022a08af4ee3968cdae3d6ee0ceed04e6381caf1
Ngawang Choeying <ngawangchoeying303@gmail.com>
2024-11-14 23:44:31 +0530

    [components::intro-card] editing intro card for better words

--- src/app/components/intro-card/intro-card.component.ts
+++ src/app/components/intro-card/intro-card.component.ts
@@ -11,7 +11,7 @@ import { RouterLink } from '@angular/router';
             <img class="rounded-full w-[55vw]" src="https://avatars.githubusercontent.com/u/77958773?v=4" alt="Ngawang Choeying profile picture" />
         </div>
         <div class="w-full h-full text-xl flex flex-col justify-center items-center lg:p-5 gap-3 lg:gap-5">
-            <p class="text-sm text-center lg:text-2xl">Hello, I'm your full-stack developer speaking
+            <p class="text-sm text-center lg:text-2xl">Hello, This is your full-stack developer speaking
             <h1 class="text-md text-center sm:text-3xl md:text-4xl text-black">Ngawang Choeying</h1>
             <p class="text-md text-center lg:text-4xl font-medium ">Call sign: sicario4l
             <!-- <section class="flex gap-5">
