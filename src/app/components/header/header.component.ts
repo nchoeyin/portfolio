a3111dcf657cf061dd98ff430fd543982b2a9ba2
Ngawang Choeying <ngawangchoeying303@gmail.com>
2025-02-07 22:28:57 +0530

    [app::header] fixed the height of the header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,21 +5,21 @@ import { CommonModule, NgIf } from '@angular/common';
   standalone: true,
   imports: [CommonModule, NgIf],
   template: `
-  <section id="navbar" class="flex justify-between h-36 items-center bg-white">
+  <section id="navbar" class="flex justify-between h-[17vh] items-center bg-white">
     <div class="text-md sm:text-2xl md:text-3xl text-black text-center font-sm">Ngawang Choeying</div>
     <section class="flex gap-4 items-center">
       <span *ngIf="!showMenu"(click)="showMenu =! showMenu" class="material-symbols-outlined text-md sm:text-3xl md:hidden lg:hidden text-black cursor-pointer">menu</span>
       <section class="md:flex gap-6 {{showMenu ? 'block' : 'hidden'}}">
-          <div class="text-xs sm:text-lg lg:text-2xl text-black hover:text-gray-200">
+          <div class="text-xs sm:text-lg lg:text-xl text-black hover:text-gray-200">
             <a href="#experience">Experience</a>
           </div>
-          <div class="text-xs sm:text-xl lg:text-2xl text-black hover:text-gray-200">
+          <div class="text-xs sm:text-xl lg:text-xl text-black hover:text-gray-200">
             <a href="#skills">Skills</a>
           </div>
-          <div class="text-xs sm:text-xl lg:text-2xl text-black hover:text-gray-200">
+          <div class="text-xs sm:text-xl lg:text-xl text-black hover:text-gray-200">
             <a href="#projects">DaggerProjects</a>
           </div>
-          <div class="text-xs sm:text-xl lg:text-2xl text-black hover:text-gray-200">
+          <div class="text-xs sm:text-xl lg:text-xl text-black hover:text-gray-200">
             <a href="#contact">Contact</a>
           </div>
       </section>
